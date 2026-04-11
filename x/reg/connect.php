99f099c532caa22cf16eb46da673661b7c4574f0
rald <thecoconutnutisagiantnut@gmail.com>
2026-04-12 06:46:02 +0800

    OK

---
+++ x/reg/connect.php
@@ -0,0 +1,27 @@
+#!php-cgi
+
+<?php
+
+$dbname = "pantasya.db";
+
+/*
+$dbhost = "sql112.epizy.com";
+$dbuser = "epiz_28817728";
+$dbpass = "gdTA6izrfFVzyA";
+$dbname = "epiz_28817728_pantasya";
+*/
+
+/*
+$dbhost = "localhost";
+$dbuser = "fria";
+$dbpass = "alohomora";
+$dbname = "pantasya";
+*/
+
+
+
+$db = new SQLite3($dbname);
+
+
+
+?>
